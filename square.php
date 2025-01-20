4b72119d7acc2eee16a2bba60651b5405d9a89b5
diyaaroraa05 <diyaaroraa18@gmail.com>
2025-01-20 20:53:36 +0530

    Delete square.php

--- square.php
+++
@@ -1,7 +0,0 @@
-<?php
-$number = 5; 
-$square = $number * $number;
-echo "The square of $number is: $square";
-echo"<br>";
-echo("This program is written and executed by Diya Arora (2220100252)");
-?>
